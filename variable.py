58141755d3050c411cc1ac198dbf82ccf5798740
pkrihnaazure <pkrishna.cap@outlook.com>
2020-08-19 15:02:24 +0530

    dlolne

--- variable.py
+++ variable.py
@@ -2,8 +2,20 @@
 x=4
 x="john"
 print(x)
-'''
+
 x = y = z = "Orange"
 print(x)
 print(y)
 print(z)
+
+x = "krishn"
+print("python is " + x)
+
+x = "Krishna"
+y = "Reddy"
+z = x +  y
+print(z)
+'''
+x =5
+y = 12
+print(x + y)
